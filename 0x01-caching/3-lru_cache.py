fcafdaa50bfdf5c911de149dd223ecaaba75b8ad
HajarElMannani <laso125@hotmail.com>
2025-05-14 19:58:57 +0000

    Change condition

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -17,15 +17,13 @@ class LRUCache(BaseCaching):
         the item value for the key key'''
         if key is None or item is None:
             return
-        if (key not in self.cache_data):
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(last=False)
-                print("DISCARD: {}".format(lru_key))
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=True)
-        else:
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=True)
+        if (key in self.cache_data):
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            lru_key, _ = self.cache_data.popitem(last=False)
+            print("DISCARD: {}".format(lru_key))
 
     def get(self, key):
         ''' return the value in self.cache_data linked to key'''
