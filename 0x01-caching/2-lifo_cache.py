5c91d1b6c875e89cd99a4a372659bcd60149e393
HajarElMannani <laso125@hotmail.com>
2025-05-14 18:38:37 +0000

    First commit

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Class LIFOCache'''
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    '''class LIFOCache that inherits from
+    BaseCaching and is a caching system'''
+    def __init__(self):
+        '''instantiation'''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''assign to the dictionary self.cache_data
+        the item value for the key key'''
+        if key is None and item is None:
+            return
+        if (
+                key not in self.cache_data and
+                len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS
+        ):
+            last_key, _ = self.cache_data.popitem()
+            print("DISCARD: {}".format(last_key))
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        ''' return the value in self.cache_data linked to key'''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
