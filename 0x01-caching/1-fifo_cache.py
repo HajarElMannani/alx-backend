4523cbfa6af261916226bb60271f309ebc4df563
HajarElMannani <laso125@hotmail.com>
2025-05-14 17:23:32 +0000

    First commit

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -14,15 +14,14 @@ class FIFOCache(BaseCaching):
         '''assign to the dictionary self.cache_data
         the item value for the key key'''
         if key is not None and item is not None:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first = next(iter(self.cache_data))
-                del self.cache_data[first]
-                print("DISCARD: {}".format(first))
             self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = next(iter(self.cache_data))
+            print("DISCARD: {}".format(first))
+            del self.cache_data[first]
 
     def get(self, key):
         ''' return the value in self.cache_data linked to key'''
         if key is None or key not in self.cache_data:
             return None
-        else:
-            return self.cache_data[key]
+        return self.cache_data[key]
