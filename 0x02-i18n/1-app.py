9f5ca20f6549241d0f2482c7472c9486edf49a36
HajarElMannani <laso125@hotmail.com>
2025-05-20 20:08:56 +0000

    Pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,18 +1,25 @@
+#!/usr/bin/env python3
+'''Flask app'''
 from flask import Flask, render_template
 from flask_babel import Babel
+
 
 class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @app.route('/')
-def index():
+def index() -> str:
+    '''render 1-index template'''
     return render_template('1-index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
