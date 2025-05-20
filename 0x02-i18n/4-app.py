45ffbe6e9e677a31bec60b9874fd321a62255591
HajarElMannani <laso125@hotmail.com>
2025-05-20 20:18:18 +0000

    Pycodestyle

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python3
+'''A Flask app'''
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
+
 
 class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale() -> str:
@@ -18,10 +23,12 @@ def get_locale() -> str:
         return lan
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def get_index() -> str:
     '''render template'''
     return render_template('4-index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
