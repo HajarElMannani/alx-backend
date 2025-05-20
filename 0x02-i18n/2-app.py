f8bac1cbb0ca7373b46c064c7bda5ee5bc9c545d
HajarElMannani <laso125@hotmail.com>
2025-05-20 09:53:22 +0100

    First commit

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    '''render template'''
+    return render_template('2-index.html')
+
+@babel.localeselector
+def get_locale():
+    '''get language from the user accept'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
